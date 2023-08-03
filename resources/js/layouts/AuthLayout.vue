834a522cca2210f9f17272eb18a6371dc57ca9cd
Felix Salcher <dev.felix.salcher@gmail.com>
2023-08-03 14:14:39 +0200

    fixed authlayout card issue

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,10 +1,9 @@
-<script setup>
-</script>
-
 <template>
 	<main class="h-screen flex items-center justify-center bg-slate-100">
 		<Card>
-			<slot />
+			<template #content>
+				<slot />
+			</template>
 		</Card>
 	</main>
 </template>
